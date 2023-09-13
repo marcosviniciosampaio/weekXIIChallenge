2bef3ac4e952ef73265ef4ddcd63282c52710476
Marcos Vinicio <marcosviniciosampaioaraujo@gmail.com>
2023-09-13 14:24:51 -0300

    msraces

---
+++ msraces/src/main/java/marcos/filho/msraces/MsracesApplication.java
@@ -0,0 +1,16 @@
+package marcos.filho.msraces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableFeignClients
+public class MsracesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsracesApplication.class, args);
+	}
+
+}
---
+++ msraces/src/test/java/marcos/filho/msraces/MsracesApplicationTests.java
@@ -0,0 +1,13 @@
+package marcos.filho.msraces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsracesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
